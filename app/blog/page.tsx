97c03ea657c3d13a469a8ebed01501f08b9b5cce
Arthur P <arthurstt22@gmail.com>
2025-09-12 12:47:26 -0300

    shipping information. - Updated header component to comment out ThemeSwitch. - Modified package.json to include only built dependencies for sharp.

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+import ListaPosts from '@/components/blog/ListaDePosts';
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <div>blog</div>
+      <ListaPosts />
+    </>
+  );
+};
+
+export default page;
